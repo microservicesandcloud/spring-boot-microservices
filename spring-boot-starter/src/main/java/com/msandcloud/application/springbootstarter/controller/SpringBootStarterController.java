79510df58019f86a29dc62f2bd59e8da39413b7e
shruthin chinni <shruthincdev@gmail.com>
2018-12-11 23:38:21 -0500

    Retuning JSON - domain object as response

--- spring-boot-starter/src/main/java/com/msandcloud/application/springbootstarter/controller/SpringBootStarterController.java
+++ spring-boot-starter/src/main/java/com/msandcloud/application/springbootstarter/controller/SpringBootStarterController.java
@@ -3,12 +3,19 @@ package com.msandcloud.application.springbootstarter.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.msandcloud.application.springbootstarter.domain.HelloWorldBean;
+
 @RestController
 public class SpringBootStarterController {
 	
 	@GetMapping(path="/helloWorld")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path="/helloWorldBean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
 	}
 
 }
---
+++ spring-boot-starter/src/main/java/com/msandcloud/application/springbootstarter/domain/HelloWorldBean.java
@@ -0,0 +1,21 @@
+package com.msandcloud.application.springbootstarter.domain;
+
+public class HelloWorldBean {
+	
+	private String message;
+	
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+
+}
